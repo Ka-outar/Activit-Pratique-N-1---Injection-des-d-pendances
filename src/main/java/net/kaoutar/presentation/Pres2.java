3dc1ece95299c51de6a00c690949569d2759555f
Ka-outar <misbahkaoutar9@gmail.com>
2026-02-22 22:08:09 +0000

    la couche presentation est fermé à la modification

--- src/main/java/net/kaoutar/presentation/Pres2.java
+++ src/main/java/net/kaoutar/presentation/Pres2.java
@@ -1,17 +1,24 @@
 package net.kaoutar.presentation;
 
 import net.kaoutar.dao.IDao;
+import net.kaoutar.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Pres2 {
-    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    //FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(new File("config.txt"));
         String daoClassName = sc.nextLine();
         Class cDao = Class.forName(daoClassName);
-        IDao dao=(IDao) cDao.newInstance();
-        System.out.println(dao.getData());
+        IDao d=(IDao) cDao.newInstance();
+
+
+
+        String metierClassName = sc.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier= (IMetier) cDao.getConstructor(IDao.class).newInstance(d);
     }
 }
