c25247cbc8cc81fafcbcc65b13613baefd089c86
Ka-outar <misbahkaoutar9@gmail.com>
2026-02-22 22:15:41 +0000

    l'injection via setter

--- src/main/java/net/kaoutar/presentation/Pres2.java
+++ src/main/java/net/kaoutar/presentation/Pres2.java
@@ -5,6 +5,7 @@ import net.kaoutar.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class Pres2 {
@@ -19,6 +20,11 @@ public class Pres2 {
 
         String metierClassName = sc.nextLine();
         Class cMetier = Class.forName(metierClassName);
-        IMetier metier= (IMetier) cDao.getConstructor(IDao.class).newInstance(d);
+        //IMetier metier= (IMetier) cDao.getConstructor(IDao.class).newInstance(d);
+        IMetier metier= (IMetier) cMetier.getConstructor().newInstance();
+        Method setDao = cMetier.getDeclaredMethod("setDao",IDao.class);
+        setDao.invoke(metier,d);
+        System.out.println("RES = "+metier.calcul());
+
     }
 }
